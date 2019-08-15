7abdef031d1a5e3fe4c396b9a09efec0effa1ff7
Adriel Dinelli <adinelli@bionexo.com>
2019-08-15 10:27:35 -0300

    asd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// //--------------------------------------------------------------------
+// // No React
+// const root = document.getElementById('root');
+//
+// function App(props) {
+//   var person = props.person;
+//
+//   var element = document.createElement('div');
+//   element.innerHTML = `Hello ${person}`;
+//   return element;
+// }
+//
+// const app = new App({ person: 'World' });
+// root.appendChild(app);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// //--------------------------------------------------------------------
+// // With React, no JSX
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// const App = ({ person }) => {
+//   return React.createElement('div', null, `Hello ${person}`);
+// };
+//
+// ReactDOM.render(
+//   React.createElement(App, {person: 'World'}, null),
+//   document.getElementById('root')
+// );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// //--------------------------------------------------------------------
+// // With React & JSX
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// const App = () => {
+//   return <div>Hello World</div>;
+// };
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
